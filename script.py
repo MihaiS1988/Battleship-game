0b08a0096e486de37ce1dfa79eee3ff1ee759704
Mihai Stan <117821906+MihaiS1988@users.noreply.github.com>
2023-03-15 16:00:16 +0000

    added the for loop for the user choice

--- script.py
+++ script.py
@@ -18,13 +18,22 @@ print("Let's play Battleship!")
 print_board(board)
 
 
+# This will define the random row from board
 def random_row(board):
     return random.randint(0, len(board) - 1)
 
 
+# This will define the random column from board
 def random_col(board):
     return random.randint(0, len(board[0]) - 1)
 
 
+# This is used to call the elements
 ship_row = random_row(board)
 ship_col = random_col(board)
+
+# This for loop is uses to ask the the user to select the column and row
+for turn in range(4):
+    print("Turn", turn + 1)
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
