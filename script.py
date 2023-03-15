9e8d66885c666ec58d411e47c05c7c76b8158dc1
Mihai Stan <117821906+MihaiS1988@users.noreply.github.com>
2023-03-15 15:56:13 +0000

    defined the row and colomn

--- script.py
+++ script.py
@@ -7,3 +7,20 @@ board = []
 # This is to loop through the range of grid system that is 5*5
 for i in range(5):
     board.append(["O"] * 5)
+
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+
+print("Let's play Battleship!")
+print_board(board)
+
+
+def random_row(board):
+    return random.randint(0, len(board) - 1)
+
+
+def random_col(board):
+    return random.randint(0, len(board[0]) - 1)
