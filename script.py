6c2efe6da07d1da15310334f4a3d9a85aba23952
MihaiS1988 <117821906+MihaiS1988@users.noreply.github.com>
2023-03-15 16:16:07 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,6 @@
-# This is to import the library
 import random
 
-# This creates a empty board that uses strings
 board = []
-
-# This is to loop through the range of grid system that is 5*5
 for i in range(5):
     board.append(["O"] * 5)
 
@@ -18,21 +14,17 @@ print("Let's play Battleship!")
 print_board(board)
 
 
-# This will define the random row from board
 def random_row(board):
     return random.randint(0, len(board) - 1)
 
 
-# This will define the random column from board
 def random_col(board):
     return random.randint(0, len(board[0]) - 1)
 
 
-# This is used to call the elements
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-# This for loop is uses to ask the the user to select the column and row
 for turn in range(4):
     print("Turn", turn + 1)
     guess_row = int(input("Guess Row: "))
@@ -52,5 +44,4 @@ for turn in range(4):
             board[guess_row][guess_col] = "X"
         if (turn == 3):
             print("Game Over")
-
-print_board(board)
+    print_board(board)
