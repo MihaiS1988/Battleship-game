f17008ae8158cda8a060bdba503287d3b1e2930f
Mihai Stan <117821906+MihaiS1988@users.noreply.github.com>
2023-03-15 15:54:40 +0000

    added script file along with a forloop

---
+++ script.py
@@ -0,0 +1,9 @@
+# This is to import the library
+import random
+
+# This creates a empty board that uses strings
+board = []
+
+# This is to loop through the range of grid system that is 5*5
+for i in range(5):
+    board.append(["O"] * 5)
